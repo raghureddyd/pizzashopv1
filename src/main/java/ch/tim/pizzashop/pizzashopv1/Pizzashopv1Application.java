7d50101cacdc74b76aea804be1db08d4d220a22c
raghavendra.dhanugon <raghavendra.dhanugonda@deltalogic.ch>
2021-06-27 18:07:11 +0200

    Project Skeleton

---
+++ src/main/java/ch/tim/pizzashop/pizzashopv1/Pizzashopv1Application.java
@@ -0,0 +1,13 @@
+package ch.tim.pizzashop.pizzashopv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pizzashopv1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pizzashopv1Application.class, args);
+	}
+
+}
